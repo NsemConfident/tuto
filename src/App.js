7ef3bb818682690f12022c9e91dc8a82cb365df0
NsemConfident <119768978+NsemConfident@users.noreply.github.com>
2023-05-22 17:09:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/header/Header'
+import Contact from './components/contact/Contact'
+
+
+function App() {
+    return ( <
+        >
+        <
+        Header / >
+        <
+        Contact / >
+
+        <
+        />
+    );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './header.css'
+import './CTA'
+import CTA from './CTA' //here we import the CTA component inside our header
+import me from '../../assets/mypic.jpg'
+import HeaderSocial from './HeaderSocial' //import of headerSocial component
+export const Header = () => {
+    return ( <
+        header >
+        <
+        div className = 'container header__container' >
+        <
+        h5 > HI ITS ME!!!! < /h5> <
+        h1 > NSEM CONFIDENT < /h1> <
+        h5 className = 'text-light' > Software Engineer < /h5> <
+        CTA / >
+        <
+        HeaderSocial / >
+        <
+        div className = 'my_image' >
+        <
+        img src = { me }
+        alt = 'my image' / >
+        <
+        /div> <
+        a href = '#contact'
+        className = 'scroll__down' > Scroll down < /a> < /
+        div > <
+        /header>
+    )
+}
+export default Header;
