bf95b5924e6b5177d0ae573b28cc26621e1d583c
NsemConfident <119768978+NsemConfident@users.noreply.github.com>
2023-06-06 03:51:39 +0100

    7th commit

--- src/components/myportfolio/Myportfolio.jsx
+++ src/components/myportfolio/Myportfolio.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './myportfolio.css'
+import img1 from '../../assets/image_1.jpg'
+import img2 from '../../assets/image_2.jpg'
+import img3 from '../../assets/image_3.jpg'
 const Myportfolio = () => {
   return (
     <section id='myportfolio'>
@@ -8,13 +11,41 @@ const Myportfolio = () => {
       <div className="container portfolio__container">
         <article className="portfolio__item">
            <div className="portfolio__item-image">
-
+           <img src={img1} alt="" />
            </div>
 
-           <h3>this is a portfolio item title  </h3>
-           <a href="https://github.com/NsemConfident" className='btn'>Github</a>
+           <h3>projects worked on</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com/NsemConfident" className='btn'>Github</a>
            <a href="https:dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a> 
            
+          </div>
+
+        </article>
+        <article className="portfolio__item">
+           <div className="portfolio__item-image">
+           <img src={img2} alt="" />
+           </div>
+
+           <h3>projects worked on</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com/NsemConfident" className='btn'>Github</a>
+           <a href="https:dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a> 
+           
+          </div>
+
+        </article>
+        <article className="portfolio__item">
+           <div className="portfolio__item-image">
+           <img src={img3} alt="" />
+           </div>
+
+           <h3>projects worked on </h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com/NsemConfident" className='btn'>Github</a>
+           <a href="https:dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a> 
+           
+          </div>
 
         </article>
       </div>
